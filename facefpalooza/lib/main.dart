411eed3d730d3daf475ce7db19b987bea51ebde3
marilzadias.correia <marilzadias.correia@gmail.com>
2024-10-10 23:22:31 +0000

    Setup do firebase

--- facefpalooza/lib/main.dart
+++ facefpalooza/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'widgets/act_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
